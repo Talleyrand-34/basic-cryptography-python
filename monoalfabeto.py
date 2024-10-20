9cf2cdb31949a6103ea4f6f3495985e479543e6d
Talleyrand-34 <talleyrand-34@tutanota.com>
2024-10-20 20:05:54 +0200

    quit write-output on stream

--- monoalfabeto.py
+++ monoalfabeto.py
@@ -153,12 +153,12 @@ def main():
         plaintext = read_file(args.e)
         ciphertext = playfair_encrypt(plaintext, key)
         print("Encrypted text:", ciphertext)
-        write_output(ciphertext, "enc")
+        #write_output(ciphertext, "enc")
     elif args.d:
         ciphertext = read_file(args.d)
         plaintext = playfair_decrypt(ciphertext, key)
         print("Decrypted text:", plaintext)
-        write_output(plaintext, "dec")
+        #write_output(plaintext, "dec")
     else:
         choice = input("Enter 'e' for encryption or 'd' for decryption (q to quit) or 'help' for help: ")
 
